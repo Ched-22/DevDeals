673191eaa604800c3856bec3674c1e76e4da4d5c
Ched-22 <pedrochede22@gmail.com>
2026-04-30 15:17:25 +0100

    inserção do número correto do wpp e logo na index.html

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,7 @@ import WhatsAppButton from './components/WhatsAppButton';
 
 export default function App() {
   // Configure o número do WhatsApp aqui (formato: 351912345678)
-  const whatsappNumber = '+351932132126';
+  const whatsappNumber = '+351963057340';
 
   const handleWhatsAppClick = (message: string) => {
     const encodedMessage = encodeURIComponent(message);
